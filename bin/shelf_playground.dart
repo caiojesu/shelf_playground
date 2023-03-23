3386da207b80677cf40052028c3802c2bb12e3e7
Caio Jesus <caioj3505@gmail.com>
2023-03-23 09:50:27 -0300

    first commit

---
+++ bin/shelf_playground.dart
@@ -0,0 +1,5 @@
+import 'package:shelf_playground/shelf_playground.dart' as shelf_playground;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shelf_playground.calculate()}!');
+}
---
+++ test/shelf_playground_test.dart
@@ -0,0 +1,8 @@
+import 'package:shelf_playground/shelf_playground.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
